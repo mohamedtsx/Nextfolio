dbbd6cafa2d87ab59f5886db022fe7781c8d07ca
Mohamed Khaled <ma0068664@gmail.com>
2023-06-18 16:13:49 +0300

    set up environment

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,14 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      colors: {
+        darkblue: {
+          0: "#1b242f",
+          1: "#252934"
+        },
+        pink: "#e31b6d",
+        golden: "#FFD100" 
+      }
     },
   },
   plugins: [],
