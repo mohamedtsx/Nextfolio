ee65db7e84bd915318f576e4b36a29e95316fde0
Mohamed Khaled <ma0068664@gmail.com>
2023-06-18 16:25:36 +0300

    add basic components

---
+++ components/header/header.component.tsx
@@ -0,0 +1,12 @@
+
+
+const Header = () => {
+
+    return(
+        <header className="bg-black h-14 flex justify-center items-center">
+            <label className="text-xl text-white">Mohamed Khalid</label>
+        </header>
+    )
+}
+
+export default Header;
