7653feb514074be874a03a12364686f987b78e39
Mohamed Khaled <ma0068664@gmail.com>
2023-06-20 12:28:04 +0300

    add humburger menu animation

--- components/drop/drop.component.tsx
+++ components/drop/drop.component.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Menu from "../menu/menu.component";
 
 const Drop = () => {
 
@@ -18,13 +19,16 @@ const Drop = () => {
 
 
     return(
-        <div onClick={() => setOpen(!open)} className={`${active? 'rounded-tl-none': ' animate-drop'}  transition-[border-radius] duration-500 ease-in-out rotate-45 fixed right-4 rounded-full  bg-darkblue-0 cursor-pointer`}>
-            <div className=" -rotate-45 flex flex-col justify-center items-center sm:hidden w-12 h-12  p-1  overflow-hidden">
-                <span className="block my-[3px] w-8 h-[3px] bg-gold"/>
-                <span className="block my-[3px] w-8 h-[3px] bg-gold"/>
-                <span className="block my-[3px] w-8 h-[3px] bg-gold"/>
+        <>
+            <div onClick={() => setOpen(!open)} className={`${active? 'rounded-tl-none': ' animate-drop'} drop-shadow-md filter-none transition-[border-radius] duration-500 ease-in-out rotate-45 fixed right-4 rounded-full  bg-darkblue-0 cursor-pointer z-10`}>
+                <div className={`relative drop ${open? 'open': ''} -rotate-45 flex flex-col justify-center items-center w-12 h-12  p-1  overflow-hidden`}>
+                    <span className="absolute top-4 w-8 h-[3px] bg-gold"/>
+                    <span className="absolute top-6 w-8 h-[3px] bg-gold"/>
+                    <span className="absolute top-8 w-8 h-[3px] bg-gold"/>
+                </div>
             </div>
-        </div>
+            {open? <Menu/> : null}
+        </>
     )
 }
 
