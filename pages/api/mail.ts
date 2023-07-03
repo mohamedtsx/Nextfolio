00aae1eab3bb446cd1ed2b2e38d79bf42977e4e6
Mohamed Khaled <ma0068664@gmail.com>
2023-07-03 16:17:44 +0300

    use sendgrid api for contact form

--- pages/api/mail.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next"
-
-
-export default (req:NextApiRequest, res: NextApiResponse) => {
-    const body = JSON.parse(req.body);
-    console.log(body);
-
-    res.status(200).json({ status: 'Ok' });
-}
